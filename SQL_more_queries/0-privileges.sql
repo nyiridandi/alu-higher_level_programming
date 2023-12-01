25421957f4e45622e923466cea559e2c1bf295a0
nyiridandi <b.rutwaza@alustudent.com>
2023-12-01 01:00:00 -0800

    Updated

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
 
-SHOW Grants for 'user_0d_1'@'localhost';
-SHOW Grants for 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
